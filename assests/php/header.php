6577c0a37a3fc38056713f39759167f61d94c886
Steven David Simmons <sds9278@g.rit.edu>
2016-05-07 17:32:30 -0400

    Content Load 3
    All pages finished in html except for the blog and the about us page.
    Will re-upload in php onto local server.

--- assests/php/header.php
+++ assests/php/header.php
@@ -1,14 +1,69 @@
-<?php
- 
-echo '
-<link rel="stylesheet" type="text/css" href="pizza.css" />
-<script type="text/javascript" src="pizza.js"></script>
+<!doctype html>
+<html lang=''>
+<head>
+   <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="styles.css">
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="script.js"></script>
+   <title>Rochester</title>
 </head>
 <body>
 
-<div id="wrapper">';
+<div class="nav-bar">
+<ul>
+   <li class="dropdown"><a href='../eats/eats-landing.html' class="dropItem"><span>Eat</span></a>
+      <div class="dropdown-content">
+         <a href='../eats/bars.html'>Bars</a>
+         <a href='../eats/ethnic.html'>Ethnic</a>
+         <a href='../eats/famous.html'>Famous</a>
+         <a href='../eats/fast.html'>Fast</a>
+         <a href='../eats/late.html'>Late</a>
+      </div>
+   </li>
+   <li><a href='../shops/shops-landing.html'><span>Shop</span></a>
+   		<div class="dropdown-content">
+	        <a href='../shops/malls.html'>Malls</a>
+	        <a href='../shops/thrift.html'>Thrift</a>
+      	</div>
+   </li>
+   <li><a href='../friends/friends-landing.html'><span>Friends</span></a>
+   		<div class="dropdown-content">
+	        <a href='../friends/free.html'>Free</a>
+	        <a href='../friends/pay-to-play.html'>Pay to Play</a>
+      	</div>
+   </li>
+   <li><a href='../entertainment/entertainment-landing.html'><span>Entertainment</span></a>
+   		<div class="dropdown-content">
+	        <a href='../entertainment/campus-events.html'>Campus</a>
+	        <a href='../entertainment/concerts.html'>Concerts</a>
+	        <a href='../entertainment/movie-theatres.html'>Movies</a>
+	        <a href='../entertainment/theatres.html'>Theatres</a>
+      	</div>
+   </li>
+   <li><a href='../go/go-landing.html'><span>Go</span></a>
+   		<div class="dropdown-content">
+	        <a href='../go/arts.html'>Arts</a>
+	        <a href='../go/festivals.html'>Festivals</a>
+	        <a href='../go/recreation.html'>Recreation</a>
+      	</div>
+   </li>
+   <li><a href='../socialize/socialize-landing.html'><span>Socialize</span></a>
+   		<div class="dropdown-content">
+	        <a href='../socialize/houses.html'>Houses</a>
+	        <a href='../socialize/nightlife.html'>Nightlife</a>
+	        <a href='../socialize/rit-clubs.html'>RIT Clubs</a>
+      	</div>
+   </li>
+   <li><a href='../history.html'><span>Rochester</span></a></li>
+   <li class="dropdown"><a href='../about-us.html' class="dropItem"><span>Us</span></a>
+      <div class="dropdown-content">
+         <a href='../blog.php'>Blog</a>
+      </div>
+   </li>
+</ul>
+</div>
 
-echo "<h1>Eat more Pizza!</h1>
-<p><img src='hotpizza.png'></p>";
-
-?>
+</body>
+<html>
